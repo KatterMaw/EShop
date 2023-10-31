74e2f412871c0228689fa3cb4ca7982be001f986
KatterMaw <narcat99@gmail.com>
2023-10-31 23:41:40 +0500

    Create EShop.Data project with initial AppDbContext implementation

---
+++ EShop.Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EShop.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.Data;
+
+public sealed class AppDbContext : DbContext
+{
+	public DbSet<User> Users { get; set; } = null!;
+	public DbSet<GuestUser> GuestUsers { get; set; } = null!;
+	public DbSet<AuthenticatedUser> AuthenticatedUsers { get; set; } = null!;
+	
+	public AppDbContext(DbContextOptions options) : base(options)
+	{
+	}
+}
