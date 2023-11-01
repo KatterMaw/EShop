8a49d31072d321fccaf6e77e87a4bfec0b363f2c
KatterMaw <narcat99@gmail.com>
2023-11-01 23:17:24 +0500

    Show sign-in button if user isn't authenticated and sign-out otherwise

--- EShop.WebAPI/Controllers/HomeController.cs
+++ EShop.WebAPI/Controllers/HomeController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EShop.Data;
+using FlakeId;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EShop.WebAPI.Controllers;
 
 public sealed class HomeController : Controller
 {
-	public IActionResult Index()
+	public HomeController(AppDbContext dbContext)
 	{
-		return View();
+		_dbContext = dbContext;
 	}
+
+	public async Task<IActionResult> Index()
+	{
+		var user = await _dbContext.Users.FindAsync(new Id(long.Parse(HttpContext.User.Claims.Single(claim => claim.Type == "Id").Value)));
+		return View(user);
+	}
+
+	private readonly AppDbContext _dbContext;
 }
